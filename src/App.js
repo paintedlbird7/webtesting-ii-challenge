c9be0c66135d47e875ca97ef97be63198de50e23
paintedlbird7 <rosaperezlarios@gmail.com>
2019-04-23 12:27:37 -0700

    fixing error

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {
+    players: [
+      // { id: 1, name: 'Sam' },
+      // { id: 2, name: 'Bilbo' },
+      // { id: 3, name: 'Frodo' },
+    ],
+  }
+
+  render() {
+  return (
+    <div className="App">
+      <h2>Hello World</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/players/Display/Display.specs.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import {render} from 'react-testing-library';
+import 'jest-dom/extend-expect';

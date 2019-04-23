4a50ff46f8c5c3925c5230c32b9ee7c63e8672e1
paintedlbird7 <rosaperezlarios@gmail.com>
2019-04-23 13:44:03 -0700

    testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import App from './App';
+import { render, cleanup } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+// afterEach(cleanup)
 
 it('renders without crashing', () => {
-
+  // render(<App />)
 });
